59ff14835a550c865ba1e5f03a1ce2e32804a047
utyvert <93745628+utyvert@users.noreply.github.com>
2024-04-10 02:17:06 -0400

    titling and renaming

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Echo",
+  title: "Echo - Waitlist",
   description: "Rank. Rate. Relive.",
   icons: {
     icon: "/EchoLarge.png",
