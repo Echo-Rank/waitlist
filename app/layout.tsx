83e0615d4f896a8c9192bbdb7e6df916b07e5417
utyvert <93745628+utyvert@users.noreply.github.com>
2025-03-06 14:14:08 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Echo",
-  description: "Rank. Rate. Relive.",
+  description: "Rank, Rate, Relisten.",
   icons: {
     icon: "/EchoLarge.png",
   },
